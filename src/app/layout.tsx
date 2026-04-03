85729dc339e33f7d58704cc1ad7c02d1f2ffe78c
mouhcineahmed0011-code <inglizi.com@192.168.1.5>
2026-04-03 23:33:55 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 import WhatsAppButton from '@/components/WhatsAppButton'
 
 export const metadata: Metadata = {
@@ -34,6 +35,8 @@ export default function RootLayout({
         />
       </head>
       <body style={{ fontFamily: "'Cairo', sans-serif" }}>
+         {children}
+    <Analytics />
         <Header />
         <main>{children}</main>
         <Footer />
